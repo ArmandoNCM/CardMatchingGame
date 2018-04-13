490d49c71f410c04fcddbdf1a56551aeac93b7f7
Armando Castillo <armando.castillo@sundevs.com>
2018-04-13 15:32:43 -0500

    Changed resources used in the game

--- config.js
+++ config.js
@@ -1,35 +1,39 @@
 var gameConfig = `{
     "columns" : 4,
     "rows" : 2,
-    "background" : "images/bg_game_1.jpg",
+    "background" : "images/bg_game_2.jpg",
     "areaMarginLateral": "10%",
     "figure": "ral",
     "backgroundCard": "images/sundevs.png",
     "winnerColor": "green",
     "backModalWinner": "images/bg-flare.jpg",
-    "titleModalWinner": "GANASTE !",
-    "textBtnModalWinner": "JUGAR DE NUEVO",
-    "backBtnModalWinner": "#D8D8D8",
+    "titleModalWinner": "Great !",
+    "textBtnModalWinner": "Play again?",
+    "backBtnModalWinner": "#fe0056",
     "cards" : [
         {
             "name": "javascript",
-            "img": "images/cards/card_booksy.png",
-            "id": 1
+            "img": "images/cards/card-2.png",
+            "id": 1,
+            "img2": "images/cards/card-two.png"
         },
         {
             "name": "css3",
-            "img": "images/cards/card_carol.png",
-            "id": 2
+            "img": "images/cards/card-4.png",
+            "id": 2,
+            "img2": "images/cards/card-four.png"
         },
         {
             "name": "python",
-            "img": "images/cards/card_drew.png",
-            "id": 3
+            "img": "images/cards/card-7.png",
+            "id": 3,
+            "img2": "images/cards/card-seven.png"
         },
         {
             "name": "Rails",
-            "img": "images/cards/card_robby.png",
-            "id": 4
+            "img": "images/cards/card-9.png",
+            "id": 4,
+            "img2": "images/cards/card-nine.png"
         }
         
     ]
